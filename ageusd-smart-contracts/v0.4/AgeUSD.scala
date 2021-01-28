83c56ef2e1de7deb7da6ebd2456d3f006a43c410
ScalaHub <23208922+scalahub@users.noreply.github.com>
2021-01-29 00:26:41 +0530

    Add non-negative check for R4, R5 of bank box out

--- ageusd-smart-contracts/v0.4/AgeUSD.scala
+++ ageusd-smart-contracts/v0.4/AgeUSD.scala
@@ -79,7 +79,8 @@ private[v4] object Contracts {
        |    val validDeltas = (scCircIn + scCircDelta == scCircOut) &&
        |                      (rcCircIn + rcCircDelta == rcCircOut) &&
        |                      (bcReserveIn + bcReserveDelta == bcReserveOut) &&
-       |                      circDelta >= $minTokenDelta && circDelta <= $maxTokenDelta
+       |                      circDelta >= $minTokenDelta && circDelta <= $maxTokenDelta &&
+       |                      scCircOut >= 0 && rcCircOut >= 0
        |
        |    val coinsConserved = totalRcIn == totalRcOut && totalScIn == totalScOut
        |
